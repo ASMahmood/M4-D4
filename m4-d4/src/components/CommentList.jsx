08d3592d52dfc97555366b30b693439623205241
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-19 15:38:21 +0000

    added comment loader

--- m4-d4/src/components/CommentList.jsx
+++ m4-d4/src/components/CommentList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Spinner } from "react-bootstrap";
 import SingleComment from "./SingleComment";
 
 class CommentList extends React.Component {
   state = {
     comments: [],
+    loading: true,
   };
 
   componentDidMount = async () => {
@@ -19,15 +20,22 @@ class CommentList extends React.Component {
         }
       );
       let comments = await response.json();
-      this.setState({ comments: comments });
+      this.setState({ comments: comments, loading: false });
     } catch (e) {
       console.log(e);
+      this.setState({ loading: false });
     }
   };
 
   render() {
     return (
       <>
+        {this.state.loading && (
+          <div className="w-100 my-2 d-flex justify-content-center">
+            <span>Loading comments...</span>
+            <Spinner animation="border" variant="success" />
+          </div>
+        )}
         <ListGroup variant="flush" className="w-100">
           {this.state.comments.map((comment, index) => (
             <SingleComment commentObj={comment} key={index} />
